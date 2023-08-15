18c5b9cc6d3ee9603d9988a53219bd60cf4f037e
Pink210 <133900344+Pink210@users.noreply.github.com>
2023-08-15 19:12:33 +0330

    Update install.bash

--- install.bash
+++ install.bash
@@ -8,13 +8,11 @@ plain='\033[0m'
 clear
 
 echo -e "${yellow}'hello' ${plain}"
-read -rep $'!!! IMPORTANT !!!\n\ndo you want continue? [[y/N]]{plain}' response
-case "$response" in
-[yY][eE][sS]|[yY])
+
 
 # Check for the Update and make a backup
 if [ -d "/bot/SoftBot" ]; then
-  echo "${red}SoftBot is already installed. The script is attempting to create a backup."
+  echo "${green}SoftBot is already installed. The script is attempting to create a backup.${plain}"
   echo "USE 'Ctrl + C' to cancel it."
   sudo systemctl stop softbot
   sleep 2
@@ -29,20 +27,26 @@ fi
 
 # Start from here
 # Perform apt update
+echo "${green}Updating Linux${plain}"
 sudo apt-get update -y && sudo apt-get -o Dpkg::Options::="--force-confold" -y upgrade -y && sudo apt-get autoremove -y 
 sleep 2
-
-# install necessary pip
-apt install python3-pip -y
+clear
+echo "${green}installing PIP${plain}"
+# install the necessary pip
+apt install python3-pip -y || exit
 sleep 2
-pip install discord.py
-pip install discord 
-pip install pandas
+pip install discord.py  || exit
+pip install discord  || exit
+pip install pandas  || exit
+pip install discord.ext
+echo "${red}IGNORE the error${plain}"
 pip install json
 pip install time
-pip install discord.ext
 
 
+clear
+echo "${green}So far so good${plain}"
+echo "${green}Download the bot${plain}"
 # Download SoftBot
 git clone https://github.com/Pink210/SoftBot.git || exit
 sleep 2
@@ -52,6 +56,7 @@ sleep 2
 sudo cp -rf /root/SoftBot/ /bot/
 sleep 2
 
+echo "${green}Create the service${plain}"
 # Create the service file with the desired content
 sudo tee /etc/systemd/system/softbot.service > /dev/null << 'EOF'
 [Unit]
@@ -78,9 +83,10 @@ sudo systemctl start softbot.service || exit
 sleep 2
 sudo systemctl stop softbot.service
 
+clear
 # Restore backup
 if [ -d "/opt/backup" ]; then
-  echo "Restoring backup."
+  echo "${green}Restoring backup.${plain}"
   sudo systemctl stop softbot.service
   sleep 2
   sudo cp -f /bot/backup/msg.py.bak /bot/softether/msg.py 
@@ -92,6 +98,6 @@ fi
 #add needrestart back again
 sudo sed -i "s/#\$nrconf{restart} = 'a';/\$nrconf{restart} = 'i';/" /etc/needrestart/needrestart.conf
 clear
-echo 'Have FUN ;).'
-echo 'First "sudo nano /bot/SoftBot/setup.py" for Config the bot'
-echo 'second "sudo systemctl restart softbot.service" to run the bot'
+echo "${green}Have FUN ;).${plain}"
+echo "${red}First 'sudo nano /bot/SoftBot/setup.py' for Config the bot${plain}"
+echo "${red}then 'sudo systemctl restart softbot.service' to run the bot${plain}"
